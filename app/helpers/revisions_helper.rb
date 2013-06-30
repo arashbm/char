7e132709d1741dfb81d9a22c6d76ff98d31a3906
Arash Badie Modiri <arashbm@gmail.com>
2013-06-30 23:54:57 +0430

    fixed a bug in revisions

--- app/helpers/revisions_helper.rb
+++ app/helpers/revisions_helper.rb
@@ -6,28 +6,30 @@ module RevisionsHelper
 
   def previous_next_btn(rev)
     previous_link =  if rev.try(:previous)
-      link_to(t('.previous_html'), post_revision_path(rev.reify, rev.previous), class: 'btn')
+      link_to(t('.previous_html'), post_revision_path(rev.item_id, rev.previous), class: 'btn')
     else
       link_to(t('.previous_html'), '#', class: 'btn disabled')
     end
     next_link = if rev.try(:next)
-      link_to(t('.next_html'), post_revision_path(rev.reify, rev.next), class: 'btn')
+      link_to(t('.next_html'), post_revision_path(rev.item_id, rev.next), class: 'btn')
     else
       link_to(t('.next_html'), '#', class: 'btn disabled')
     end
     "<div class='btn-group'>#{previous_link}#{next_link}</div>".html_safe
   end
 
-  def originator_name(rev)
-    rev.originator ? User.find(rev.originator).name : t('.unknown_originator')
+  def terminator_name(rev)
+    rev.terminator ? User.find(rev.terminator.to_i).name : t('.unknown_terminator')
   end
 
   def diff(rev, attribute)
-    diff = if rev.changeset.include? attribute.to_s
-      simple_format(show_diff(*(rev.changeset[attribute.to_s])))
-    else
-      simple_format(rev.reify[attribute.to_sym])
-    end
+    diff = if rev.event == 'create'
+             simple_format(rev.changeset[attribute.to_s][1])
+           elsif rev.changeset.include? attribute.to_s
+             simple_format(show_diff(*(rev.changeset[attribute.to_s])))
+           else
+             simple_format(rev.reify[attribute.to_sym])
+           end
     diff.html_safe
   end
 
